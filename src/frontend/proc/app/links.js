07b28af4320c31c406d6af40d8863269bcdc60fa
Oigres2 <sergiovb2002@hotmail.com>
2023-12-26 14:46:58 +0000

    feat: oldest sold car page

--- src/frontend/proc/app/links.js
+++ src/frontend/proc/app/links.js
@@ -5,6 +5,7 @@ const LINKS = [
     {text: 'Home', href: '/', icon: HomeIcon},
     {text: 'Brands', href: '/brands', icon: People },
     {text: 'Models', href: '/car_models', icon: People },
-    {text: 'Sales Per Country', href: '/sales_per_country', icon: People }
+    {text: 'Sales Per Country', href: '/sales_per_country', icon: People },
+    {text: 'Oldest Sold Car', href: '/oldest_sold_car', icon: People }
 ];
 export default LINKS;
