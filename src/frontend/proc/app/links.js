7474712fbce5798f37075d030563849ec2f9fc7e
Oigres2 <sergiovb2002@hotmail.com>
2023-12-30 22:09:35 +0000

    feat: page most sold brands

--- src/frontend/proc/app/links.js
+++ src/frontend/proc/app/links.js
@@ -8,6 +8,7 @@ const LINKS = [
     {text: 'Sales Per Country', href: '/sales_per_country', icon: People },
     {text: 'Oldest Sold Car', href: '/oldest_sold_car', icon: People },
     {text: 'Newest Sold Car', href: '/newest_sold_car', icon: People },
-    {text: 'Most Sold Colors', href: '/most_sold_colors', icon: People }
+    {text: 'Most Sold Colors', href: '/most_sold_colors', icon: People },
+    {text: 'Most Sold Brands', href: '/most_sold_brands', icon: People }
 ];
 export default LINKS;
