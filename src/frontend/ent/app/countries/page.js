abf4a182d90bbe28de74a94b8e6f09e2208a0c05
Diogo Bernardes <Diogobernardes@ipvc.pt>
2023-12-30 15:56:34 +0000

    Feat: Country table listing

--- src/frontend/ent/app/countries/page.js
+++ src/frontend/ent/app/countries/page.js
@@ -1,3 +1,75 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import {
+  CircularProgress,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material";
+import useApi from "../Hooks/useAPI";
+
 export default function CountriesPage() {
-    return <main><b>Countries Page</b>: Work in progress</main>;
+  const api = useApi();
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await api.GET("/countries");
+      setCountries(response.data);
+    } catch (error) {
+      console.error("Error fetching countries:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const renderCountryRows = () => {
+    return countries.map((country) => (
+      <TableRow key={country.id}>
+        <TableCell component="td" align="center">
+          {country.id}
+        </TableCell>
+        <TableCell component="td" scope="row">
+          {country.name}
+        </TableCell>
+      </TableRow>
+    ));
+  };
+
+  return (
+    <main>
+      <h1>
+        <b>Countries Page</b>
+      </h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{ backgroundColor: "lightgray" }}>
+              <TableCell component="th" width={"1px"} align="center">
+                ID
+              </TableCell>
+              <TableCell>Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {countries.length > 0 ? (
+              renderCountryRows()
+            ) : (
+              <TableRow>
+                <TableCell colSpan={2} align="center">
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </main>
+  );
 }
