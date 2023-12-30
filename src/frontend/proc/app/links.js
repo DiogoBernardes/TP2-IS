253037d0584485b83f7f23c9dd9d4e747a347573
Oigres2 <sergiovb2002@hotmail.com>
2023-12-30 22:25:29 +0000

    feat: page most sold models

--- src/frontend/proc/app/links.js
+++ src/frontend/proc/app/links.js
@@ -9,6 +9,7 @@ const LINKS = [
     {text: 'Oldest Sold Car', href: '/oldest_sold_car', icon: People },
     {text: 'Newest Sold Car', href: '/newest_sold_car', icon: People },
     {text: 'Most Sold Colors', href: '/most_sold_colors', icon: People },
-    {text: 'Most Sold Brands', href: '/most_sold_brands', icon: People }
+    {text: 'Most Sold Brands', href: '/most_sold_brands', icon: People },
+    {text: 'Most Sold Models', href: '/most_sold_models', icon: People }
 ];
 export default LINKS;
---
+++ src/frontend/proc/app/most_sold_models/page.js
@@ -0,0 +1,65 @@
+// ./app/top_teams/page.js
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Container,
+  CircularProgress,
+} from "@mui/material";
+import useAPI from "../Hooks/useAPI";
+
+function MostSoldModels() {
+  const { GET } = useAPI();
+  const [carData, setCarData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    GET('/mostSoldBrands')
+      .then((result) => {
+        if (result.data) {
+          setCarData(result.data);
+        } else {
+          setCarData(null);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setCarData(null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Most Sold Models</h1>
+      <Container
+        maxWidth="100%"
+        sx={{
+          backgroundColor: "info.dark",
+          padding: "2rem",
+          marginTop: "2rem",
+          borderRadius: "1rem",
+          color: "white",
+        }}
+      >
+        <h2>Results <small>(PROC)</small></h2>
+        {loading ? (
+          <CircularProgress />
+        ) : carData ? (
+          <Box>
+            {Object.entries(carData).map(([key, value]) => (
+              <p key={key}>{`${key}: ${value}`}</p>
+            ))}
+          </Box>
+        ) : (
+          <p>No data available</p>
+        )}
+      </Container>
+    </>
+  );
+}
+
+export default MostSoldModels;
