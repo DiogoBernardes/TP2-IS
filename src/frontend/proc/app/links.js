5c118456b3ddfac179b235d8a97ed98b1d6babb1
Oigres2 <sergiovb2002@hotmail.com>
2024-01-01 22:37:46 +0000

    feat: changed page icons

--- src/frontend/proc/app/links.js
+++ src/frontend/proc/app/links.js
@@ -1,15 +1,15 @@
 import HomeIcon from "@mui/icons-material/Home";
-import {People} from "@mui/icons-material";
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
 
 const LINKS = [
     {text: 'Home', href: '/', icon: HomeIcon},
-    {text: 'Brands', href: '/brands', icon: People },
-    {text: 'Models', href: '/car_models', icon: People },
-    {text: 'Sales Per Country', href: '/sales_per_country', icon: People },
-    {text: 'Oldest/Newest Sold Car', href: '/oldest_newest_sold_car', icon: People },
-    {text: 'Most Sold Colors', href: '/most_sold_colors', icon: People },
-    {text: 'Most Sold Brands', href: '/most_sold_brands', icon: People },
-    {text: 'Most Sold Models', href: '/most_sold_models', icon: People },
-    {text: 'Car Sales Per Year', href: '/car_sales_year', icon: People }
+    {text: 'Brands', href: '/brands', icon: TimeToLeaveIcon },
+    {text: 'Models', href: '/car_models', icon: TimeToLeaveIcon },
+    {text: 'Sales Per Country', href: '/sales_per_country', icon: TimeToLeaveIcon },
+    {text: 'Oldest/Newest Sold Car', href: '/oldest_newest_sold_car', icon: TimeToLeaveIcon },
+    {text: 'Most Sold Colors', href: '/most_sold_colors', icon: TimeToLeaveIcon },
+    {text: 'Most Sold Brands', href: '/most_sold_brands', icon: TimeToLeaveIcon },
+    {text: 'Most Sold Models', href: '/most_sold_models', icon: TimeToLeaveIcon },
+    {text: 'Car Sales Per Year', href: '/car_sales_year', icon: TimeToLeaveIcon }
 ];
 export default LINKS;
