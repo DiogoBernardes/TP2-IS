4cd2d437cebb88c9e6b9344fd6bf683ce880cb6f
kromenz <rafael.pires.andre@ipvc.pt>
2024-01-15 23:57:18 +0000

    api-gis, gis-updater, frontend-gis (countries page) done

--- src/daemon/gis-updater/main.py
+++ src/daemon/gis-updater/main.py
@@ -1,13 +1,80 @@
 import sys
 import time
+from flask import json
 import pika
+import requests
 
+API_BASE_URL = "http://api-gis:8080"
 POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 30
 ENTITIES_PER_ITERATION = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 
+def fetch_data_from_api(endpoint):
+    try:
+        response = requests.get(f"{API_BASE_URL}/{endpoint}")
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from API: {e}")
+        return None
+
+def get_coordinates_by_country_name(country_name):
+    base_url = "https://nominatim.openstreetmap.org/search"
+    
+    params = {
+        "q": country_name,
+        "format": "json",
+        "limit": 1
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if data:
+            latitude = data[0]["lat"]
+            longitude = data[0]["lon"]
+            geojson = {
+                "type": "Point",
+                "coordinates": [float(longitude), float(latitude)]
+            }
+            return geojson
+        else:
+            print(f"Não foi possível encontrar coordenadas para o país: {country_name}")
+            return None
+    except Exception as e:
+        print(f"Erro ao obter coordenadas: {str(e)}")
+        return None
+
 def callback(ch, method, properties, body):
-    # Este é o callback que será chamado quando uma mensagem for recebida da fila
+
     print(f"Received message: {body.decode()}")
+
+    countries_data = fetch_data_from_api("api/countries")
+    if countries_data:
+        # Processa dados dos países
+        for country in countries_data:
+            country_name = country.get("name")
+
+            if country_name:
+                # Obtém coordenadas
+                coordinates = get_coordinates_by_country_name(country_name)
+
+                if coordinates:
+                    country_id = country.get("id")
+                    update_url = f"{API_BASE_URL}/api/countries/{country_id}"
+                    update_data = {"geom": json.dumps(coordinates)}
+
+                    try:
+                        response = requests.put(update_url, json=update_data)
+                        response.raise_for_status()
+                        print(f"Country: {country_name}, Coordinates: {coordinates} - Coordinates updated successfully.")
+                    except requests.exceptions.RequestException as e:
+                        print(f"Failed to update coordinates for {country_name}: {e}")
+            else:
+                print("Country name is missing in the data.")
+
+    else:
+        print("Failed to fetch data from API.")
 
 def consume_messages():
     # Conectar ao RabbitMQ
--- src/frontend/gis/app/countries/page.js
+++ src/frontend/gis/app/countries/page.js
@@ -1,3 +1,53 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import Paper from "@mui/material/Paper";
 export default function CountriesPage() {
-    return <main><b>Countries Page</b>: Work in progress</main>;
+  const [countries, setCountries] = useState([]);
+
+  const fetchCountries = async (countryId) => {
+    try {
+      const response = await fetch("http://localhost:20002/api/countries");
+      const data = await response.json();
+      const sortedCountries = data.sort((a, b) => a.name.localeCompare(b.name));
+      setCountries(sortedCountries);
+    } catch (error) {
+      console.error("Erro ao buscar modelos para o país:", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <main>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Name</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {countries.map((country) => (
+              <TableRow key={country.id}>
+                <TableCell>{country.id}</TableCell>
+                <TableCell>{country.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </main>
+  );
 }
