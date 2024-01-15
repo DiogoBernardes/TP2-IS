b47f8f40f540c3235fe15a1c2f1bfdea27b93666
kromenz <rafael.pires.andre@ipvc.pt>
2024-01-15 13:17:12 +0000

    gis-updater receives msg

--- src/daemon/gis-updater/main.py
+++ src/daemon/gis-updater/main.py
@@ -1,14 +1,39 @@
 import sys
 import time
+import pika
 
-POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 60
+POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 30
 ENTITIES_PER_ITERATION = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 
-if __name__ == "__main__":
+def callback(ch, method, properties, body):
+    # Este é o callback que será chamado quando uma mensagem for recebida da fila
+    print(f"Received message: {body.decode()}")
 
+def consume_messages():
+    # Conectar ao RabbitMQ
+    credentials = pika.PlainCredentials('is', 'is')
+    parameters = pika.ConnectionParameters('rabbitmq', 5672, 'is', credentials=credentials)
+
+    try:
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+
+        # Declare a fila 'geom' com durabilidade consistente
+        channel.queue_declare(queue='geom', durable=True)
+
+        # Configurar o callback para processar as mensagens
+        channel.basic_consume(queue='geom', on_message_callback=callback, auto_ack=True)
+
+        print(f"Waiting for messages from the 'geom' queue...")
+        # Iniciar a escuta de mensagens
+        channel.start_consuming()
+
+    except pika.exceptions.ProbableAccessDeniedError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
     while True:
-        print(f"Getting up to {ENTITIES_PER_ITERATION} entities without coordinates...")
-        # !TODO: 1- Use api-gis to retrieve a fixed amount of entities without coordinates (e.g. 100 entities per iteration, use ENTITIES_PER_ITERATION)
-        # !TODO: 2- Use the entity information to retrieve coordinates from an external API
-        # !TODO: 3- Submit the changes
+        # Consumir mensagens
+        consume_messages()
+
         time.sleep(POLLING_FREQ)
